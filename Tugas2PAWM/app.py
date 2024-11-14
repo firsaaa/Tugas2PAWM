97c7889e08aaa4d50e7aa96fd41f884fd27351be
firsaaa <firsa.raissa@gmail.com>
2024-11-15 00:26:22 +0700

    cbcb

--- Tugas2PAWM/app.py
+++ Tugas2PAWM/app.py
@@ -1,8 +1,8 @@
 from app import create_app
 from app.routes.progress_routes import progress_bp
 
-app.register_blueprint(progress_bp, url_prefix='/progress')
 app = create_app()
+app.register_blueprint(progress_bp, url_prefix='/progress')
 
 if __name__ == "__main__":
     app.run(debug=True)

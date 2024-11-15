73cc8d6e06bc9f5d07d5e83713be9db3374cfe87
firsaaa <firsa.raissa@gmail.com>
2024-11-15 17:59:04 +0700

    update

--- Tugas2PAWM/app.py
+++ Tugas2PAWM/app.py
@@ -1,4 +1,6 @@
 from app import app
+from flask import Flask
+from flask_cors import CORS
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Tugas2PAWM/config.py
+++ Tugas2PAWM/config.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    MONGO_URI = os.getenv("MONGO_URI")
+    SECRET_KEY = os.getenv("SECRET_KEY")
